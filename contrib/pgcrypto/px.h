0f81f7839cb9904380d1c42581963f40eca31e8d
Daniel Gustafsson <dgustafsson@pivotal.io>
2019-08-15 01:51:17 +0200

    Support FIPS mode operation in pgcrypto
    In Greenplum 4.3 there was a GUC in pgcrypto controlling FIPS mode
    operation which was deprecated. This brings the GUC back, as there
    has been requests from production instances.
    
    To enable the GUC, Greenplum must be dynamically linked against an
    OpenSSL installation which is compiled with the FIPS module (1.0.2
    with FIPS 2.0 is tested and supported, no FIPS module exist for
    later versions of OpenSSL as of yet). There is an additional strict
    mode as well which requires both OpenSSL to be FIPS capable as well
    as the operating system kernel to be running in FIPS mode (this is
    only supported on Linux for now).
    
    Some ciphers will be disallowed in this mode even though they are
    defined as legal by FIPS-140 level 2, due to the fact that pgcrypto
    is using the lowlevel API for these and this is not certified for
    FIPS operation.

--- contrib/pgcrypto/px.h
+++ contrib/pgcrypto/px.h
@@ -202,6 +202,9 @@ void		px_set_debug_handler(void (*handler) (const char *));
 
 void		px_memset(void *ptr, int c, size_t len);
 
+void		px_enable_fipsmode(bool strict);
+void		px_disable_fipsmode(void);
+
 #ifdef PX_DEBUG
 void
 px_debug(const char *fmt,...)
